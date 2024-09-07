ae030596e5782754fd5265378992bf7578115044
K1002H <32428193+K1002H@users.noreply.github.com>
2024-09-07 15:28:16 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -36,6 +36,7 @@ UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 UPDATE_PACKAGE "pgyvpn" "kiddin9/openwrt-packages" "master" "pkg"
 UPDATE_PACKAGE "phtunnel" "kiddin9/openwrt-packages" "master" "pkg"
 UPDATE_PACKAGE "wrtbwmon" "kiddin9/openwrt-packages" "master" "pkg"
+UPDATE_PACKAGE "lucky" "kiddin9/openwrt-packages" "master" "pkg"
 #UPDATE_PACKAGE "shortcut-fe" "kiddin9/openwrt-packages" "master" "pkg"
 #UPDATE_PACKAGE "fancontrol" "kiddin9/openwrt-packages" "master" "pkg"
 
@@ -52,6 +53,7 @@ UPDATE_PACKAGE "luci-app-webadmin" "kiddin9/openwrt-packages" "master" "pkg"
 UPDATE_PACKAGE "luci-app-broadbandacc" "kiddin9/openwrt-packages" "master" "pkg"
 #UPDATE_PACKAGE "luci-app-fan" "kiddin9/openwrt-packages" "master" "pkg"
 UPDATE_PACKAGE "luci-app-v2ray-server" "kiddin9/openwrt-packages" "master" "pkg"
+UPDATE_PACKAGE "luci-app-lucky" "kiddin9/openwrt-packages" "master" "pkg"
 
 if [[ $WRT_REPO == *"openwrt-6.x"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
